7a640b7fee66ba3a24e110cca4da8605b7cb34dd
Qian Chen <elgs1980@hotmail.com>
2022-01-22 13:33:43 -0800

    Update.

--- splitpane/splitpane.js
+++ splitpane/splitpane.js
@@ -6,7 +6,7 @@ export class SplitPane {
    static id = 'azui-splitpane';
    static settings = {
       direction: 'v', // v for vertical or h for horizonal
-      hideCollapseButton: true
+      // hideCollapseButton: true
    };
 
    init() {
@@ -20,22 +20,28 @@ export class SplitPane {
       const parts = dom.children.length;
       const partSizePercent = 100.0 / parts;
 
-      const colors = ['red', 'green', 'blue'];
+      // const colors = ['red', 'green', 'blue'];
       let selfSize;
       let nextSize;
+
+      const handleBorder = me.settings.direction === 'v' ? 's' : 'e';
 
       const wrappers = [...dom.children].map(function (child, index) {
          const childWrapper = document.createElement('div');
 
          az.ui(Resizable, childWrapper, {
-            handles: index < parts - 1 ? 's' : '',
+            handles: index < parts - 1 ? handleBorder : '',
             handleSize,
-            hideCollapseButton: settings.hideCollapseButton,
+            hideCollapseButton: true,
             create: function (e, h) {
                const nextWrapper = wrappers[index + 1];
-               selfSize = getHeight(childWrapper) - handleSize;
-               nextSize = getHeight(nextWrapper) - (index === parts - 2 ? 0 : handleSize);
-               console.log(index, parts);
+               if (settings.direction === 'v') {
+                  selfSize = getHeight(childWrapper) - handleSize;
+                  nextSize = getHeight(nextWrapper) - (index === parts - 2 ? 0 : handleSize);
+               } else {
+                  selfSize = getWidth(childWrapper) - handleSize;
+                  nextSize = getWidth(nextWrapper) - (index === parts - 2 ? 0 : handleSize);
+               }
                const nextResizable = nextWrapper[Resizable.id];
                nextResizable.setup();
                dom.querySelectorAll('iframe').forEach(iframe => {
@@ -43,10 +49,16 @@ export class SplitPane {
                });
             },
             resize: function (e, h, by) {
-               by.dy = Math.max(by.dy, -selfSize);
-               by.dy = Math.min(by.dy, nextSize);
                const nextResizable = wrappers[index + 1][Resizable.id];
-               nextResizable.moveN(by.dy);
+               if (settings.direction === 'v') {
+                  by.dy = Math.max(by.dy, -selfSize);
+                  by.dy = Math.min(by.dy, nextSize);
+                  nextResizable.moveN(by.dy);
+               } else {
+                  by.dx = Math.max(by.dx, -selfSize);
+                  by.dx = Math.min(by.dx, nextSize);
+                  nextResizable.moveW(by.dx);
+               }
             },
             stop: function (e, el) {
                dom.querySelectorAll('iframe').forEach(iframe => {
@@ -59,11 +71,18 @@ export class SplitPane {
                });
             }
          });
-         childWrapper.classList.add('azSplitPane');
-         childWrapper.style['background-color'] = colors[index % colors.length];
+         // childWrapper.classList.add('azSplitPane');
+         // childWrapper.style['background-color'] = colors[index % colors.length];
          childWrapper.style.position = 'absolute';
-         childWrapper.style.top = partSizePercent * index + '%';
-         childWrapper.style.height = partSizePercent + '%';
+         if (me.settings.direction === 'v') {
+            childWrapper.style.top = partSizePercent * index + '%';
+            childWrapper.style.height = partSizePercent + '%';
+            childWrapper.style.width = '100%';
+         } else {
+            childWrapper.style.left = partSizePercent * index + '%';
+            childWrapper.style.width = partSizePercent + '%';
+            childWrapper.style.height = '100%';
+         }
          childWrapper.style.overflow = 'auto';
          me.dom.appendChild(childWrapper);
          childWrapper.appendChild(child);
